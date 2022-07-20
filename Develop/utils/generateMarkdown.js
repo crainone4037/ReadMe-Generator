5b7c3e2810b5027b2f2b78c5b2ca875735d96066
crainone4037 <rainonecarmine0@gmail.com>
2022-07-20 13:43:31 -0400

    generateMarkDown page

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,28 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const generateMarkdown = answers => {
+`
+# ${answers.title}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+#description
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+${answers.descrption}
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+## Installation
+${answers.installation}
 
-`;
+## Usage
+
+${answers.usage}
+
+
+
+
+
+
+`
 }
+
+
 
 module.exports = generateMarkdown;
